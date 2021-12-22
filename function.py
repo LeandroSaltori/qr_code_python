d2e2871447c66ae6f40ead45425519f41a8475c6
LeandroSB <leandrosb@hotmail.com>
2021-12-22 18:46:20 -0300

    update funçao

---
+++ function.py
@@ -0,0 +1,18 @@
+def qrcode(exten, password, domain, port=5060):
+    img_qrcode = qrcode.make(f"""
+    <?xml version="1.0" encoding='utf-8'?>
+    <AccountConfig version='1'>
+        <Account>
+            <RegisterServer>{domain}:{port}</RegisterServer>"
+            <UserID>{exten}</UserID>
+            <AuthID>{exten}</AuthID>
+            <AuthPass>{password}</AuthPass>
+            <AccountName>{exten}</AccountName>
+            <DisplayName>{exten}</DisplayName>
+            <Voicemail>*97</Voicemail>
+            </Account>
+    </AccountConfig>
+    """)
+
+    type(img_qrcode)  # qrcode.image.pil.PilImage
+    return img_qrcode.save("qrcode.png")
