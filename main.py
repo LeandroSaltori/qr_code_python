c2816ce27df006d787f1b46eaf562602384ae0ed
LeandroSaltori <leandro@prismatelecom.com>
2021-12-22 16:39:25 -0300

    commit correcao

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import qrcode
 
-import qrcode
-img = qrcode.make('Some data here')
+#name = str(input('Digite o nome do usuário do ramal: '))
+exten = str(input('Digite o número do ramal: '))
+password = str(input('Digite a senha do ramal: '))
+domain = str(input('Digite o IP/DDNS: '))
+port = 5060
+# correio de voz = 
+
+img = qrcode.make(f"""
+<?xml version="1.0" encoding='utf-8'?>
+<AccountConfig version='1'>
+	<Account>
+		<RegisterServer>{exten}:{port}</RegisterServer>"
+		<UserID>{exten}</UserID>
+		<AuthID>{exten}</AuthID>
+		<AuthPass>{password}</AuthPass>
+		<AccountName>{exten}</AccountName>
+		<DisplayName>{exten}</DisplayName>
+		<Voicemail>*97</Voicemail>
+		</Account>
+</AccountConfig>
+""")
+
 type(img)  # qrcode.image.pil.PilImage
 img.save("qrcode.png")
