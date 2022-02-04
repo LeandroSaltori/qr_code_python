321a48a487da03cb5b8d131733c421395f643cb5
LeandroSaltori <leandro@prismatelecom.com>
2022-02-04 18:19:08 -0300

    update

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from cgitb import text
-from turtle import bgcolor
 from function import gerar_qrcode
 import tkinter as tk
 
@@ -25,22 +23,36 @@ janela.columnconfigure([0, 1], weight=1)
 mensagem = tk.Label(text='Sistema Gerar QRCode Prisma Telecom', font='bold',fg='white', bg='black', width='35', height='1') # Fonte = Batman Forever
 mensagem.grid(row=0, column=0, columnspan=2, sticky='NSEW')
 
+#Nome Ramal
 name_texto = tk.Label(text='Digite o nome do usuário do ramal: ')
 name_texto.grid(row=1, column=0)
-
 name = tk.Entry()
 name.grid(row=1, column=1)
 
-# exten = tk.Label(text='Digite o nome do usuário do ramal: ')
-# exten.pack()
+#Numero Ramal
+exten_texto = tk.Label(text='Digite o número do ramal: ')
+exten_texto.grid(row=2, column=0)
+exten = tk.Entry()
+exten.grid(row=2, column=1)
 
-# password = tk.Label(text='Digite o nome do usuário do ramal: ')
-# password.pack()
+#Senha do ramal
+password_texto = tk.Label(text='Digite a senha do ramal ')
+password_texto.grid(row=3, column=0)
+password = tk.Entry()
+password.grid(row=3, column=1)
 
-# domain = tk.Label(text='Digite o nome do usuário do ramal: ')
-# domain.pack()
+#IP / Dominio de registo do ramal
+domain_texto = tk.Label(text='IP/ Dominio:  ')
+domain_texto.grid(row=4, column=0)
+domain = tk.Entry()
+domain.grid(row=4, column=1)
 
-# port = tk.Label(text='Digite o nome do usuário do ramal: ')
-# port.pack()
+
+#Gerar Botão
+botao = tk.Button(text='Gerar QR Code', command=gerar_qrcode)
+botao.grid(row=7, column=0, columnspan=2, sticky='NSEW')
+
+
+
 
 janela.mainloop()
